86456fe9d091871e29e9b006b9eff7d80a74006a
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scbtechx.io>
2022-05-16 15:20:06 +0700

    e-market: initial project

---
+++ opn-e-market/opn-e-market/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  opn-e-market
+//
+//  Created by Phonthep Aungkanukulwit on 16/5/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
